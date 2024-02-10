030d8b37960b8dd1141d6992cd5e2dbc2af40356
Анастасия Кулакова <k.annstas@gmail.com>
2024-02-10 16:18:34 +0300

    Добавьте файлы проекта.

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,48 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfRoutedEvent_2
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void StackPanel_Click(object sender, RoutedEventArgs e)
+        {
+            FrameworkElement frameworkSource = e.Source as FrameworkElement;
+            double number = Double.Parse(txtBox.Text);
+
+            switch (frameworkSource.Name)
+            {
+                case "butAdd":
+                    number += number;
+                    break;
+                case "butMult":
+                    number*= number; 
+                    break;
+                case "butSquare":
+                    number = Math.Sqrt(number); 
+                    break;
+            }
+
+            e.Handled = true;
+
+            txtBox.Text = String.Format($"{number:#.##}");
+
+        }
+    }
+}
